924125252e3524f5330ef5b306aa61168cf6ec1c
Sanskriti <asanskriti93@gmail.com>
2026-05-18 14:51:43 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,10 @@ connectDB();
 
 app.use(cors());
 app.use(express.json());
+app.get("/", (req, res) => {
 
+  res.send("AI Employee Backend Running");
+});
 app.use("/api/employees", employeeRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/ai", aiRoutes);
