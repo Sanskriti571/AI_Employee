0978cc6c0b31bc58ff5f698c469bfcbc4a95b6da
Sanskriti <asanskriti93@gmail.com>
2026-05-18 14:36:43 +0530

    hi

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,55 +1,121 @@
 const User = require("../models/User");
+
 const bcrypt = require("bcryptjs");
+
 const jwt = require("jsonwebtoken");
+
+// SIGNUP
 
 exports.signup = async (req, res) => {
 
-  const { name, email, password } = req.body;
+  try {
 
-  const existingUser = await User.findOne({ email });
+    const {
+      name,
+      email,
+      password
+    } = req.body;
 
-  if (existingUser) {
-    return res.status(400).json({
-      message: "User already exists"
+    const existingUser =
+      await User.findOne({ email });
+
+    if (existingUser) {
+
+      return res.status(400).json({
+
+        message: "User already exists"
+      });
+    }
+
+    const hashedPassword =
+      await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+
+      name,
+
+      email,
+
+      password: hashedPassword
+    });
+
+    res.status(201).json({
+
+      message: "Registration successful",
+
+      user
+    });
+
+  } catch (error) {
+
+    console.log(error);
+
+    res.status(500).json({
+
+      message: "Server Error"
     });
   }
-
-  const hashedPassword = await bcrypt.hash(password, 10);
-
-  const user = await User.create({
-    name,
-    email,
-    password: hashedPassword
-  });
-
-  res.status(201).json(user);
 };
+
+// LOGIN
 
 exports.login = async (req, res) => {
 
-  const { email, password } = req.body;
+  try {
 
-  const user = await User.findOne({ email });
+    const {
+      email,
+      password
+    } = req.body;
 
-  if (!user) {
-    return res.status(400).json({
-      message: "Invalid credentials"
+    const user =
+      await User.findOne({ email });
+
+    if (!user) {
+
+      return res.status(400).json({
+
+        message: "Invalid credentials"
+      });
+    }
+
+    const isMatch =
+      await bcrypt.compare(
+
+        password,
+
+        user.password
+      );
+
+    if (!isMatch) {
+
+      return res.status(401).json({
+
+        message: "Unauthorized"
+      });
+    }
+
+    const token = jwt.sign(
+
+      { id: user._id },
+
+      process.env.JWT_SECRET,
+
+      { expiresIn: "7d" }
+    );
+
+    res.json({
+
+      token
+    });
+
+  } catch (error) {
+
+    console.log(error);
+
+    res.status(500).json({
+
+      message: "Server Error"
     });
   }
-
-  const isMatch = await bcrypt.compare(password, user.password);
-
-  if (!isMatch) {
-    return res.status(401).json({
-      message: "Unauthorized"
-    });
-  }
-
-  const token = jwt.sign(
-    { id: user._id },
-    process.env.JWT_SECRET,
-    { expiresIn: "7d" }
-  );
-
-  res.json({ token });
 };
