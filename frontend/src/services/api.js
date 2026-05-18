b438243b190516c9fd81d81182e6984868db8f09
Sanskriti <asanskriti93@gmail.com>
2026-05-18 14:33:15 +0530

    final

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://employee-ai-backend-oawc.onrender.com"
+  baseURL: "https://employee-ai-backend-oawc.onrender.com/api"
 });
 
 API.interceptors.request.use((req) => {
